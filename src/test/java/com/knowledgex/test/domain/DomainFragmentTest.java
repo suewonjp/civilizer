777cf93f2dad5fb23a31992dffa423c35f560a75
suewonjp <suewonjp@yahoo.co.jp>
2015-02-02 21:06:30 +0900

    [update][test] sorting fragments by date field

--- src/test/java/com/knowledgex/test/domain/DomainFragmentTest.java
+++ src/test/java/com/knowledgex/test/domain/DomainFragmentTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.*;
 
 import java.util.*;
 
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeComparator;
 import org.apache.commons.logging.Log;
 import org.junit.Before;
 import org.junit.Test;
@@ -61,6 +63,44 @@ public class DomainFragmentTest {
 		Collection<String> expectedC = buildFragmentTitleList(fragments);
 		Object[] expected = expectedC.toArray();
 		assertArrayEquals(expected, actual);
+	}
+	
+	@Test
+	public void testSortByDate() {
+	    for (Fragment f : fragments) {
+            assertNotNull(f.getCreationDatetime());
+            assertNotNull(f.getUpdateDatetime());
+        }
+	    
+	    Comparator<Fragment> cmptrForCreationTime =
+	            FragmentComparator.newComparatorForCreationDatetime();
+	    assertNotNull(cmptrForCreationTime);
+	    Comparator<Fragment> cmptrForUpdateTime =
+	            FragmentComparator.newComparatorForUpdateDatetime();
+	    assertNotNull(cmptrForUpdateTime);
+	    DateTimeComparator dtCmptr = DateTimeComparator.getInstance();
+	    
+	    Collections.shuffle(fragments);
+	    Collections.sort(fragments, cmptrForCreationTime);
+	    for (int i=1; i<fragments.size(); ++i) {
+	        Fragment f0 = fragments.get(i - 1);
+	        Fragment f1 = fragments.get(i);
+	        DateTime dt0 = f0.getCreationDatetime();
+	        DateTime dt1 = f1.getCreationDatetime();
+	        int r = dtCmptr.compare(dt0, dt1);
+	        assertTrue(r <= 0);
+	    }
+
+	    Collections.shuffle(fragments);
+	    Collections.sort(fragments, cmptrForUpdateTime);
+	    for (int i=1; i<fragments.size(); ++i) {
+	        Fragment f0 = fragments.get(i - 1);
+	        Fragment f1 = fragments.get(i);
+	        DateTime dt0 = f0.getUpdateDatetime();
+	        DateTime dt1 = f1.getUpdateDatetime();
+	        int r = dtCmptr.compare(dt0, dt1);
+	        assertTrue(r <= 0);
+	    }
 	}
 
 }
