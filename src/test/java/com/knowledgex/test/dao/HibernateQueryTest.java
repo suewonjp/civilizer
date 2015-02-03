f72df0de31bf2969d9189784b837177582f26bbb
suewonjp <suewonjp@yahoo.co.jp>
2015-02-03 20:42:01 +0900

    [update][test] basic Hibernate Criteria test

--- src/test/java/com/knowledgex/test/dao/HibernateQueryTest.java
+++ src/test/java/com/knowledgex/test/dao/HibernateQueryTest.java
@@ -9,6 +9,10 @@ import java.util.*;
 import org.apache.commons.logging.Log;
 import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.orm.hibernate3.SessionFactoryUtils;
+import org.hibernate.Criteria;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Restrictions;
+import org.hibernate.Hibernate;
 import org.hibernate.SessionFactory;
 import org.hibernate.Session;
 import org.joda.time.DateTime;
@@ -57,24 +61,54 @@ public class HibernateQueryTest {
 		session = SessionFactoryUtils.getSession(sessionFactory, true);
 		assertNotNull(session);
 	}
+	
+	@Test
+	@SuppressWarnings("unchecked")
+	public void testSimpleCriteriaQuery() {
+		Criteria crit = session.createCriteria(Fragment.class);
+		List<Fragment> fragments = (List<Fragment>) crit.list();
+		assertNotNull(fragments);
+		assertFalse(fragments.isEmpty());
+		for (Fragment f : fragments) {
+			assertNotNull(f.getId());
+			assertTrue(Hibernate.isInitialized(f.getTags()));
+		}
+		
+		final long id = 1;
+		crit.add(Restrictions.eq("id", id));
+		Fragment f0 = (Fragment) crit.uniqueResult();
+		Fragment f1 = fragmentDao.findById(id);
+		assertEquals(f0, f1);
+	}
 
 	@Test
 	@SuppressWarnings("unchecked")
-	public void testHqlOrderBy() {
-		List<Fragment> fragments = (List<Fragment>)
+	public void testCriteriaOrderBy() {
+		List<Fragment> fragments0 = (List<Fragment>)
 				session
 				.createQuery("from Fragment f order by updateDatetime desc")
 				.list();
 		
-		// [NOTE] 'order by' can't be parameterized...
-		// you can't use parameters for columns, only values
+		assertNotNull(fragments0);
+		assertFalse(fragments0.isEmpty());
 		
-		assertNotNull(fragments);
-		assertFalse(fragments.isEmpty());
+		Criteria crit = session.createCriteria(Fragment.class);
+		crit.addOrder(Order.desc("updateDatetime"));
+		crit.setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY);
+		List<Fragment> fragments1 = (List<Fragment>) crit.list();
 		
-		for (int i=1; i<fragments.size(); ++i) {
-            Fragment f0 = fragments.get(i - 1);
-            Fragment f1 = fragments.get(i);
+		assertEquals(fragments0.size(), fragments1.size());
+		
+		for (int i=0; i<fragments1.size(); ++i) {
+            Fragment f0 = fragments0.get(i);
+            Fragment f1 = fragments1.get(i);
+            log.info("i="+i+", id0="+f0.getId()+",id1="+f1.getId());
+            assertEquals(f0, f1);
+		}
+		
+		for (int i=1; i<fragments1.size(); ++i) {
+            Fragment f0 = fragments1.get(i - 1);
+            Fragment f1 = fragments1.get(i);
             DateTime dt0 = f0.getUpdateDatetime();
             DateTime dt1 = f1.getUpdateDatetime();
             int r = dtCmptr.compare(dt0, dt1);
