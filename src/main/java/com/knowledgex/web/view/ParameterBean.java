9a95a243d05ceb03cb8da5d4101c7065dd9c77cf
suewonjp <suewonjp@gmail.com>
2015-02-15 16:16:51 +0900

    [update] panel activation button logic; [todo] store panel info using client storage APIs

--- src/main/java/com/knowledgex/web/view/ParameterBean.java
+++ src/main/java/com/knowledgex/web/view/ParameterBean.java
@@ -1,11 +1,19 @@
 package com.knowledgex.web.view;
 
 import java.io.Serializable;
+import java.util.*;
 
 @SuppressWarnings("serial")
 public class ParameterBean implements Serializable {
 	
 	private int panelId = 0;
+	
+	private List<Integer> activePanels;
+	
+	ParameterBean() {
+	    activePanels = new ArrayList<Integer>();
+	    activePanels.add(0);
+	}
 
 	public int getPanelId() {
 		return panelId;
@@ -14,5 +22,13 @@ public class ParameterBean implements Serializable {
 	public void setPanelId(int panelId) {
 		this.panelId = panelId;
 	}
+
+    public List<Integer> getActivePanels() {
+        return activePanels;
+    }
+
+    public void setActivePanels(List<Integer> activePanels) {
+        this.activePanels = activePanels;
+    }
 
 }
