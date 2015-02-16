95a85429d1276424e163aee5538643d6ff13d932
suewonjp <suewonjp@yahoo.co.jp>
2015-02-16 20:39:55 +0900

    [delete] dead code

--- src/main/java/com/knowledgex/web/view/ParameterBean.java
+++ src/main/java/com/knowledgex/web/view/ParameterBean.java
@@ -8,11 +8,7 @@ public class ParameterBean implements Serializable {
 	
 	private int panelId = 0;
 	
-//	private List<Integer> activePanels;
-	
 	ParameterBean() {
-//	    activePanels = new ArrayList<Integer>();
-//	    activePanels.add(0);
 	}
 
 	public int getPanelId() {
@@ -22,13 +18,5 @@ public class ParameterBean implements Serializable {
 	public void setPanelId(int panelId) {
 		this.panelId = panelId;
 	}
-
-//    public List<Integer> getActivePanels() {
-//        return activePanels;
-//    }
-//
-//    public void setActivePanels(List<Integer> activePanels) {
-//        this.activePanels = activePanels;
-//    }
 
 }
