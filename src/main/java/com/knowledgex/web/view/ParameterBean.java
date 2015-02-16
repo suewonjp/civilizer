b93271a67a2fb874681f825e2daee19e31703a67
suewonjp <suewonjp@yahoo.co.jp>
2015-02-16 19:48:31 +0900

    [update] panel activation button logic; now implemented with jQueryUI instead of Primefaces

--- src/main/java/com/knowledgex/web/view/ParameterBean.java
+++ src/main/java/com/knowledgex/web/view/ParameterBean.java
@@ -1,18 +1,18 @@
 package com.knowledgex.web.view;
 
 import java.io.Serializable;
-import java.util.*;
+//import java.util.*;
 
 @SuppressWarnings("serial")
 public class ParameterBean implements Serializable {
 	
 	private int panelId = 0;
 	
-	private List<Integer> activePanels;
+//	private List<Integer> activePanels;
 	
 	ParameterBean() {
-	    activePanels = new ArrayList<Integer>();
-	    activePanels.add(0);
+//	    activePanels = new ArrayList<Integer>();
+//	    activePanels.add(0);
 	}
 
 	public int getPanelId() {
@@ -23,12 +23,12 @@ public class ParameterBean implements Serializable {
 		this.panelId = panelId;
 	}
 
-    public List<Integer> getActivePanels() {
-        return activePanels;
-    }
-
-    public void setActivePanels(List<Integer> activePanels) {
-        this.activePanels = activePanels;
-    }
+//    public List<Integer> getActivePanels() {
+//        return activePanels;
+//    }
+//
+//    public void setActivePanels(List<Integer> activePanels) {
+//        this.activePanels = activePanels;
+//    }
 
 }
