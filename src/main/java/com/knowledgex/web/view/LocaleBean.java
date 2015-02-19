de188d3fb4c198d78a08904beaa55e2fd11f33f6
suewonjp <suewonjp@yahoo.co.jp>
2015-02-19 23:00:01 +0900

    [new][class] LocaleBean; its purpose is to manage locale related data

---
+++ src/main/java/com/knowledgex/web/view/LocaleBean.java
@@ -0,0 +1,34 @@
+package com.knowledgex.web.view;
+
+import java.util.Locale;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+@ManagedBean
+@SessionScoped
+public class LocaleBean {
+
+    private Locale locale;
+
+    @PostConstruct
+    public void init() {
+        locale = FacesContext.getCurrentInstance().getViewRoot().getLocale();
+    }
+
+    public Locale getLocale() {
+        return locale;
+    }
+
+    public String getLanguage() {
+        return locale.getLanguage();
+    }
+
+    public void setLanguage(String language) {
+        locale = new Locale(language);
+        FacesContext.getCurrentInstance().getViewRoot().setLocale(locale);
+    }
+
+}
