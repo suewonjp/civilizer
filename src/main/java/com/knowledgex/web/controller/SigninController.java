841f4b9cebe44e735aa866c03e6c11f18d79d1c3
suewonjp <suewonjp@gmail.com>
2015-02-23 02:25:24 +0900

    [update] sign in feature; a lot more to go;

--- src/main/java/com/knowledgex/web/controller/SigninController.java
+++ src/main/java/com/knowledgex/web/controller/SigninController.java
@@ -2,8 +2,6 @@ package com.knowledgex.web.controller;
 
 import java.io.IOException;
 
-import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 import javax.servlet.RequestDispatcher;
@@ -13,17 +11,13 @@ import javax.servlet.ServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-//import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.knowledgex.web.view.*;
 
 @Controller
-//@Component("signinController")
-@ManagedBean(name="signinController")
-@RequestScoped
+@Component("signinController")
 public class SigninController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(SigninController.class);
@@ -33,13 +27,12 @@ public class SigninController {
     }
     
     public String handleSignin() throws ServletException, IOException {
-        ExternalContext context = FacesContext.getCurrentInstance().getExternalContext();
+        final ExternalContext context = FacesContext.getCurrentInstance().getExternalContext();
+        final ServletRequest req = (ServletRequest) context.getRequest();
+        final ServletResponse res = (ServletResponse) context.getResponse();
+        final RequestDispatcher dispatcher = req.getRequestDispatcher("/j_spring_security_check");
 
-        RequestDispatcher dispatcher = ((ServletRequest) context.getRequest())
-                .getRequestDispatcher("/j_spring_security_check");
-
-        dispatcher.forward((ServletRequest) context.getRequest(),
-                (ServletResponse) context.getResponse());
+        dispatcher.forward(req, res);
 
         FacesContext.getCurrentInstance().responseComplete();
         
@@ -48,9 +41,9 @@ public class SigninController {
         return null;
     }
     
-    @RequestMapping(value = "/signin", method = { RequestMethod.GET })
-    public String onSignIn() {
-	    return "signin";
-    }
+//    @RequestMapping(value = "/signin", method = { RequestMethod.GET })
+//    public String onSignIn() {
+//	    return "signin";
+//    }
 
 }
