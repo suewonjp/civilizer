443e5a8733ff6d2e91382c11b5860694382354d7
suewonjp <suewonjp@gmail.com>
2015-02-22 14:44:30 +0900

    [delete] unnecessary import

--- src/main/java/com/knowledgex/web/controller/MainController.java
+++ src/main/java/com/knowledgex/web/controller/MainController.java
@@ -12,7 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
