1fadd7cef02c8632885ef46615696e7af2d82560
suewonjp <suewonjp@yahoo.co.jp>
2015-02-25 15:52:21 +0900

    [delete] deleted unused fields of Fragment and FRAGMENT2FRAGMENT table

--- src/main/resources/db/test-data.sql
+++ src/main/resources/db/test-data.sql
@@ -2,13 +2,13 @@ INSERT INTO GLOBAL_SETTING(SETTING_NAME, SETTING_VALUE) VALUES
 ('database.version', '1')
 ;
 
-INSERT INTO FRAGMENT(FRAGMENT_ID, TITLE, CONTENT, CREATION_DATETIME, UPDATE_DATETIME, FILE_NAME, FILE_TYPE, FILE_SIZE, CHILDREN_ORDERED_BY, CHILDREN_ORDERED_IN_ASC, PASSWORD, CREATOR, UPDATER, TAG_ID) VALUES
+INSERT INTO FRAGMENT(FRAGMENT_ID, TITLE, CONTENT, CREATION_DATETIME, UPDATE_DATETIME) VALUES
 
 (1, 'bold', STRINGDECODE('
     **bold** or __bold__
 * * *
 **bold** or __bold__
-'), TIMESTAMP '2014-08-25 03:39:15.1', TIMESTAMP '2014-08-25 03:39:15.1', NULL, NULL, NULL, 1, FALSE, NULL, 'owner', NULL, NULL)
+'), TIMESTAMP '2014-08-25 03:39:15.1', TIMESTAMP '2014-08-25 03:39:15.1')
 ,(2, 'Blockquotes', STRINGDECODE('
     > Email-style angle brackets
     > are used for blockquotes.
@@ -25,19 +25,19 @@ INSERT INTO FRAGMENT(FRAGMENT_ID, TITLE, CONTENT, CREATION_DATETIME, UPDATE_DATE
 > >
 > * You can quote a list.
 > * Etc.
-'), TIMESTAMP '2014-11-03 19:05:59.121', TIMESTAMP '2014-11-03 19:10:25.797', NULL, NULL, NULL, 1, FALSE, NULL, 'owner', 'owner', NULL)
+'), TIMESTAMP '2014-11-03 19:05:59.121', TIMESTAMP '2014-11-03 19:10:25.797')
 
 ,(3, 'Links - inline', STRINGDECODE('
     [text](http://url.com/ "Title (optional)")
 * * *
 H2 [Quickstart](http://www.h2database.com/html/quickstart.html "H2 Quickstart")
-'), TIMESTAMP '2014-11-03 19:07:00.324', TIMESTAMP '2014-11-03 19:07:00.324', NULL, NULL, NULL, 1, FALSE, NULL, 'owner', NULL, NULL)
+'), TIMESTAMP '2014-11-03 19:07:00.324', TIMESTAMP '2014-11-03 19:07:00.324')
 
 ,(4, 'Images - inline', STRINGDECODE('
     ![alt text](/path/to/image "Title (optional)")
 * * *
 ![batman](https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTk-E0CVdOAiWPGhFwNQKUuTX-qZM2E0PQAebU0U45sl-6mt69h1w "Batman")
-'), TIMESTAMP '2014-11-03 19:08:24.763', TIMESTAMP '2014-11-03 19:08:24.763', NULL, NULL, NULL, 1, FALSE, NULL, 'owner', NULL, NULL)
+'), TIMESTAMP '2014-11-03 19:08:24.763', TIMESTAMP '2014-11-03 19:08:24.763')
 
 ,(5, 'Headers : Setext-style', STRINGDECODE('
         Header 1
@@ -50,21 +50,21 @@ Header 1
 ========
 \r\n
 Header 2\r\n--------
-'), TIMESTAMP '2014-11-03 19:12:44.752', TIMESTAMP '2014-11-03 19:12:44.752', NULL, NULL, NULL, 1, FALSE, NULL, 'owner', NULL, NULL)
+'), TIMESTAMP '2014-11-03 19:12:44.752', TIMESTAMP '2014-11-03 19:12:44.752')
 
-,(6, 'Trashed', STRINGDECODE('    This is a trashed fragment...'), TIMESTAMP '2014-11-03 19:12:44.752', TIMESTAMP '2014-11-03 19:12:44.752', NULL, NULL, NULL, 1, FALSE, NULL, 'owner', NULL, NULL)
+,(6, 'Trashed', STRINGDECODE('    This is a trashed fragment...'), TIMESTAMP '2014-11-03 19:12:44.752', TIMESTAMP '2014-11-03 19:12:44.752')
 ,(7, 'Lists - ordered', STRINGDECODE('
     1.  Foo
     1.  Bar\r\n---
 1.  Foo
 1.  Bar
-'), TIMESTAMP '2015-01-13 14:32:00.000', TIMESTAMP '2015-01-13 14:32:00.000', NULL, NULL, NULL, 1, FALSE, NULL, 'owner', NULL, NULL)
+'), TIMESTAMP '2015-01-13 14:32:00.000', TIMESTAMP '2015-01-13 14:32:00.000')
 
 ,(8, 'Italic', STRINGDECODE('
     *italic* or _italic_
 * * *
 *italic* or _italic_
-'), TIMESTAMP '2015-01-13 14:32:00.000', TIMESTAMP '2015-01-13 14:32:00.000', NULL, NULL, NULL, 1, FALSE, NULL, 'owner', NULL, NULL)
+'), TIMESTAMP '2015-01-13 14:32:00.000', TIMESTAMP '2015-01-13 14:32:00.000')
 
 ,(9, 'Links - reference style', STRINGDECODE('
     H2 [Quickstart][id]. Then, anywhere else in the doc, define the link:
@@ -74,7 +74,7 @@ Header 2\r\n--------
 * * *
 H2 [Quickstart][id].\r\n\r\n
 [id]: http://www.h2database.com/html/quickstart.html  "H2 Quickstart"
-'), TIMESTAMP '2014-11-03 19:07:00.324', TIMESTAMP '2014-11-03 19:07:00.324', NULL, NULL, NULL, 1, FALSE, NULL, 'owner', NULL, NULL)
+'), TIMESTAMP '2014-11-03 19:07:00.324', TIMESTAMP '2014-11-03 19:07:00.324')
 
 ,(10, 'Images - reference style', STRINGDECODE('
     ![alt text][id]  
@@ -84,7 +84,7 @@ H2 [Quickstart][id].\r\n\r\n
 * * *
 ![batman][id]\r\n\r\n
 [id]: https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTk-E0CVdOAiWPGhFwNQKUuTX-qZM2E0PQAebU0U45sl-6mt69h1w "Batman"
-'), TIMESTAMP '2014-11-03 19:08:24.763', TIMESTAMP '2014-11-03 19:08:24.763', NULL, NULL, NULL, 1, FALSE, NULL, 'owner', NULL, NULL)
+'), TIMESTAMP '2014-11-03 19:08:24.763', TIMESTAMP '2014-11-03 19:08:24.763')
 
 ,(11, 'Headers : atx-style', STRINGDECODE('
         (closing # is optional):
@@ -93,7 +93,7 @@ H2 [Quickstart][id].\r\n\r\n
 * * *
 # Header 1
 ## Header 2
-'), TIMESTAMP '2014-11-03 19:12:44.752', TIMESTAMP '2014-11-03 19:12:44.752', NULL, NULL, NULL, 1, FALSE, NULL, 'owner', NULL, NULL)
+'), TIMESTAMP '2014-11-03 19:12:44.752', TIMESTAMP '2014-11-03 19:12:44.752')
 
 ,(12, 'Lists - unordered', STRINGDECODE('
         Unordered, with paragraphs: ("*" or "+" or "-" can be used)
@@ -104,7 +104,7 @@ H2 [Quickstart][id].\r\n\r\n
 *   A list item.
     With multiple paragraphs.
 *   Bar
-'), TIMESTAMP '2014-11-03 19:12:44.752', TIMESTAMP '2014-11-03 19:12:44.752', NULL, NULL, NULL, 1, FALSE, NULL, 'owner', NULL, NULL)
+'), TIMESTAMP '2014-11-03 19:12:44.752', TIMESTAMP '2014-11-03 19:12:44.752')
 
 ,(13, 'Lists - mixed & nested', STRINGDECODE('
         You can nest them:
@@ -125,14 +125,14 @@ H2 [Quickstart][id].\r\n\r\n
         * A nested unordered list
     0. This is the last nested ordered list item
 *   The last unordered list item
-'), TIMESTAMP '2014-11-03 19:12:44.752', TIMESTAMP '2014-11-03 19:12:44.752', NULL, NULL, NULL, 1, FALSE, NULL, 'owner', NULL, NULL)
+'), TIMESTAMP '2014-11-03 19:12:44.752', TIMESTAMP '2014-11-03 19:12:44.752')
 
 ,(14, 'Code span', STRINGDECODE('
     `<code>` spans are delimited by backticks. ( You can include literal backticks like `` `this` ``. )
     Use the `printf()` function.
 * * *
 Use the `printf()` function.
-'), TIMESTAMP '2014-11-03 19:12:44.752', TIMESTAMP '2014-11-03 19:12:44.752', NULL, NULL, NULL, 1, FALSE, NULL, 'owner', NULL, NULL)
+'), TIMESTAMP '2014-11-03 19:12:44.752', TIMESTAMP '2014-11-03 19:12:44.752')
 
 ,(15, 'Etc. markdown syntax', STRINGDECODE('
 ### Preformatted Code Blocks
@@ -145,25 +145,190 @@ Use the `printf()` function.
     - - - -
 ### Manual Line Breaks
     End a line with two or more spaces:
-'), TIMESTAMP '2014-11-03 19:12:44.752', TIMESTAMP '2014-11-03 19:12:44.752', NULL, NULL, NULL, 1, FALSE, NULL, 'owner', NULL, NULL)
-,(16, 'Trashed 2', STRINGDECODE('    This is another trashed fragment...'), TIMESTAMP '2014-11-03 19:12:44.752', TIMESTAMP '2015-02-06 12:32:00.000', NULL, NULL, NULL, 1, FALSE, NULL, 'owner', NULL, NULL)
+'), TIMESTAMP '2014-11-03 19:12:44.752', TIMESTAMP '2014-11-03 19:12:44.752')
+,(16, 'Trashed 2', STRINGDECODE('    This is another trashed fragment...'), TIMESTAMP '2014-11-03 19:12:44.752', TIMESTAMP '2015-02-06 12:32:00.000')
 ;
+--INSERT INTO GLOBAL_SETTING(SETTING_NAME, SETTING_VALUE) VALUES
+--('database.version', '1')
+--;
+--
+--INSERT INTO FRAGMENT(FRAGMENT_ID, TITLE, CONTENT, CREATION_DATETIME, UPDATE_DATETIME, FILE_NAME, FILE_TYPE, FILE_SIZE, CHILDREN_ORDERED_BY, CHILDREN_ORDERED_IN_ASC, PASSWORD, CREATOR, UPDATER, TAG_ID) VALUES
+--
+--(1, 'bold', STRINGDECODE('
+--    **bold** or __bold__
+--* * *
+--**bold** or __bold__
+--'), TIMESTAMP '2014-08-25 03:39:15.1', TIMESTAMP '2014-08-25 03:39:15.1', NULL, NULL, NULL, 1, FALSE, NULL, 'owner', NULL, NULL)
+--,(2, 'Blockquotes', STRINGDECODE('
+--    > Email-style angle brackets
+--    > are used for blockquotes.
+--    > > And, they can be nested.
+--
+--    > >
+--    > * You can quote a list.
+--    > * Etc.
+--* * *
+--> Email-style angle brackets
+--> are used for blockquotes.
+--> > And, they can be nested.
+--
+--> >
+--> * You can quote a list.
+--> * Etc.
+--'), TIMESTAMP '2014-11-03 19:05:59.121', TIMESTAMP '2014-11-03 19:10:25.797', NULL, NULL, NULL, 1, FALSE, NULL, 'owner', 'owner', NULL)
+--
+--,(3, 'Links - inline', STRINGDECODE('
+--    [text](http://url.com/ "Title (optional)")
+--* * *
+--H2 [Quickstart](http://www.h2database.com/html/quickstart.html "H2 Quickstart")
+--'), TIMESTAMP '2014-11-03 19:07:00.324', TIMESTAMP '2014-11-03 19:07:00.324', NULL, NULL, NULL, 1, FALSE, NULL, 'owner', NULL, NULL)
+--
+--,(4, 'Images - inline', STRINGDECODE('
+--    ![alt text](/path/to/image "Title (optional)")
+--* * *
+--![batman](https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTk-E0CVdOAiWPGhFwNQKUuTX-qZM2E0PQAebU0U45sl-6mt69h1w "Batman")
+--'), TIMESTAMP '2014-11-03 19:08:24.763', TIMESTAMP '2014-11-03 19:08:24.763', NULL, NULL, NULL, 1, FALSE, NULL, 'owner', NULL, NULL)
+--
+--,(5, 'Headers : Setext-style', STRINGDECODE('
+--        Header 1
+--        ========
+--
+--        Header 2
+--        --------
+--* * *
+--Header 1
+--========
+--\r\n
+--Header 2\r\n--------
+--'), TIMESTAMP '2014-11-03 19:12:44.752', TIMESTAMP '2014-11-03 19:12:44.752', NULL, NULL, NULL, 1, FALSE, NULL, 'owner', NULL, NULL)
+--
+--,(6, 'Trashed', STRINGDECODE('    This is a trashed fragment...'), TIMESTAMP '2014-11-03 19:12:44.752', TIMESTAMP '2014-11-03 19:12:44.752', NULL, NULL, NULL, 1, FALSE, NULL, 'owner', NULL, NULL)
+--,(7, 'Lists - ordered', STRINGDECODE('
+--    1.  Foo
+--    1.  Bar\r\n---
+--1.  Foo
+--1.  Bar
+--'), TIMESTAMP '2015-01-13 14:32:00.000', TIMESTAMP '2015-01-13 14:32:00.000', NULL, NULL, NULL, 1, FALSE, NULL, 'owner', NULL, NULL)
+--
+--,(8, 'Italic', STRINGDECODE('
+--    *italic* or _italic_
+--* * *
+--*italic* or _italic_
+--'), TIMESTAMP '2015-01-13 14:32:00.000', TIMESTAMP '2015-01-13 14:32:00.000', NULL, NULL, NULL, 1, FALSE, NULL, 'owner', NULL, NULL)
+--
+--,(9, 'Links - reference style', STRINGDECODE('
+--    H2 [Quickstart][id]. Then, anywhere else in the doc, define the link:
+--
+--    [id]: http://www.h2database.com/html/quickstart.html  "H2 Quickstart"
+--
+--* * *
+--H2 [Quickstart][id].\r\n\r\n
+--[id]: http://www.h2database.com/html/quickstart.html  "H2 Quickstart"
+--'), TIMESTAMP '2014-11-03 19:07:00.324', TIMESTAMP '2014-11-03 19:07:00.324', NULL, NULL, NULL, 1, FALSE, NULL, 'owner', NULL, NULL)
+--
+--,(10, 'Images - reference style', STRINGDECODE('
+--    ![alt text][id]  
+--
+--    [id]: /path/to/image "Title"
+--
+--* * *
+--![batman][id]\r\n\r\n
+--[id]: https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTk-E0CVdOAiWPGhFwNQKUuTX-qZM2E0PQAebU0U45sl-6mt69h1w "Batman"
+--'), TIMESTAMP '2014-11-03 19:08:24.763', TIMESTAMP '2014-11-03 19:08:24.763', NULL, NULL, NULL, 1, FALSE, NULL, 'owner', NULL, NULL)
+--
+--,(11, 'Headers : atx-style', STRINGDECODE('
+--        (closing # is optional):
+--        # Header 1 #
+--        ## Header 2
+--* * *
+--# Header 1
+--## Header 2
+--'), TIMESTAMP '2014-11-03 19:12:44.752', TIMESTAMP '2014-11-03 19:12:44.752', NULL, NULL, NULL, 1, FALSE, NULL, 'owner', NULL, NULL)
+--
+--,(12, 'Lists - unordered', STRINGDECODE('
+--        Unordered, with paragraphs: ("*" or "+" or "-" can be used)
+--        *   A list item.
+--            With multiple paragraphs.
+--        *   Bar
+--* * *
+--*   A list item.
+--    With multiple paragraphs.
+--*   Bar
+--'), TIMESTAMP '2014-11-03 19:12:44.752', TIMESTAMP '2014-11-03 19:12:44.752', NULL, NULL, NULL, 1, FALSE, NULL, 'owner', NULL, NULL)
+--
+--,(13, 'Lists - mixed & nested', STRINGDECODE('
+--        You can nest them:
+--        *   An unordered list item
+--            * A nested unordered list item
+--        *   Another list item
+--            0.  The first nested ordered list item
+--            0.  A second nested ordered list item
+--                * A nested unordered list
+--            0. This is the last nested ordered list item
+--        *   The last unordered list item
+--* * *
+--*   An unordered list item
+--    * A nested unordered list item
+--*   Another list item
+--    0.  The first nested ordered list item
+--    0.  A second nested ordered list item
+--        * A nested unordered list
+--    0. This is the last nested ordered list item
+--*   The last unordered list item
+--'), TIMESTAMP '2014-11-03 19:12:44.752', TIMESTAMP '2014-11-03 19:12:44.752', NULL, NULL, NULL, 1, FALSE, NULL, 'owner', NULL, NULL)
+--
+--,(14, 'Code span', STRINGDECODE('
+--    `<code>` spans are delimited by backticks. ( You can include literal backticks like `` `this` ``. )
+--    Use the `printf()` function.
+--* * *
+--Use the `printf()` function.
+--'), TIMESTAMP '2014-11-03 19:12:44.752', TIMESTAMP '2014-11-03 19:12:44.752', NULL, NULL, NULL, 1, FALSE, NULL, 'owner', NULL, NULL)
+--
+--,(15, 'Etc. markdown syntax', STRINGDECODE('
+--### Preformatted Code Blocks
+--    Indent every line of a code block by at least 4 spaces or 1 tab
+--
+--### Horizontal Rules
+--    Three or more dashes or asterisks:
+--    ---
+--    * * *
+--    - - - -
+--### Manual Line Breaks
+--    End a line with two or more spaces:
+--'), TIMESTAMP '2014-11-03 19:12:44.752', TIMESTAMP '2014-11-03 19:12:44.752', NULL, NULL, NULL, 1, FALSE, NULL, 'owner', NULL, NULL)
+--,(16, 'Trashed 2', STRINGDECODE('    This is another trashed fragment...'), TIMESTAMP '2014-11-03 19:12:44.752', TIMESTAMP '2015-02-06 12:32:00.000', NULL, NULL, NULL, 1, FALSE, NULL, 'owner', NULL, NULL)
+--;
 
-INSERT INTO TAG(TAG_ID, TAG_NAME, CREATION_DATETIME, UPDATE_DATETIME, CREATOR, UPDATER, FRAGMENT_ID) VALUES
-(0, '#trash', TIMESTAMP '2015-01-13 14:23:00.000', TIMESTAMP '2015-01-13 14:23:00.000', 'system', NULL, NULL)
-,(1, 'emphasis', TIMESTAMP '2014-08-25 03:39:15.106', TIMESTAMP '2014-08-25 03:39:15.106', 'owner', NULL, NULL)
-,(2, 'blockquotes', TIMESTAMP '2014-11-03 19:05:59.134', TIMESTAMP '2014-11-03 19:05:59.134', 'owner', NULL, NULL)
-,(3, 'links', TIMESTAMP '2014-11-03 19:07:00.33', TIMESTAMP '2014-11-03 19:09:38.441', 'owner', 'owner', NULL)
-,(4, 'images', TIMESTAMP '2014-11-03 19:08:24.767', TIMESTAMP '2014-11-03 19:08:24.767', 'owner', NULL, NULL)
-,(5, 'headers', TIMESTAMP '2014-11-03 19:08:24.767', TIMESTAMP '2014-11-03 19:08:24.767', 'owner', NULL, NULL)
-,(6, 'lists', TIMESTAMP '2014-11-03 19:08:24.767', TIMESTAMP '2014-11-03 19:08:24.767', 'owner', NULL, NULL)
-,(7, 'code spans', TIMESTAMP '2014-11-03 19:08:24.767', TIMESTAMP '2014-11-03 19:08:24.767', 'owner', NULL, NULL)
-,(8, 'empty', TIMESTAMP '2014-11-03 19:08:24.767', TIMESTAMP '2014-11-03 19:08:24.767', 'owner', NULL, NULL)
-,(11, 'inline', TIMESTAMP '2014-11-03 19:08:24.767', TIMESTAMP '2014-11-03 19:08:24.767', 'owner', NULL, NULL)
-,(12, 'reference', TIMESTAMP '2014-11-03 19:08:24.767', TIMESTAMP '2014-11-03 19:08:24.767', 'owner', NULL, NULL)
-,(13, 'etc', TIMESTAMP '2014-11-03 19:08:24.767', TIMESTAMP '2014-11-03 19:08:24.767', 'owner', NULL, NULL)
-,(100, 'markdown', TIMESTAMP '2014-11-03 19:08:24.767', TIMESTAMP '2014-11-03 19:08:24.767', 'owner', NULL, NULL)
+INSERT INTO TAG(TAG_ID, TAG_NAME, CREATION_DATETIME, UPDATE_DATETIME) VALUES
+(0, '#trash', TIMESTAMP '2015-01-13 14:23:00.000', TIMESTAMP '2015-01-13 14:23:00.000')
+,(1, 'emphasis', TIMESTAMP '2014-08-25 03:39:15.106', TIMESTAMP '2014-08-25 03:39:15.106')
+,(2, 'blockquotes', TIMESTAMP '2014-11-03 19:05:59.134', TIMESTAMP '2014-11-03 19:05:59.134')
+,(3, 'links', TIMESTAMP '2014-11-03 19:07:00.33', TIMESTAMP '2014-11-03 19:09:38.441')
+,(4, 'images', TIMESTAMP '2014-11-03 19:08:24.767', TIMESTAMP '2014-11-03 19:08:24.767')
+,(5, 'headers', TIMESTAMP '2014-11-03 19:08:24.767', TIMESTAMP '2014-11-03 19:08:24.767')
+,(6, 'lists', TIMESTAMP '2014-11-03 19:08:24.767', TIMESTAMP '2014-11-03 19:08:24.767')
+,(7, 'code spans', TIMESTAMP '2014-11-03 19:08:24.767', TIMESTAMP '2014-11-03 19:08:24.767')
+,(8, 'empty', TIMESTAMP '2014-11-03 19:08:24.767', TIMESTAMP '2014-11-03 19:08:24.767')
+,(11, 'inline', TIMESTAMP '2014-11-03 19:08:24.767', TIMESTAMP '2014-11-03 19:08:24.767')
+,(12, 'reference', TIMESTAMP '2014-11-03 19:08:24.767', TIMESTAMP '2014-11-03 19:08:24.767')
+,(13, 'etc', TIMESTAMP '2014-11-03 19:08:24.767', TIMESTAMP '2014-11-03 19:08:24.767')
+,(100, 'markdown', TIMESTAMP '2014-11-03 19:08:24.767', TIMESTAMP '2014-11-03 19:08:24.767')
 ;
+--INSERT INTO TAG(TAG_ID, TAG_NAME, CREATION_DATETIME, UPDATE_DATETIME, CREATOR, UPDATER, FRAGMENT_ID) VALUES
+--(0, '#trash', TIMESTAMP '2015-01-13 14:23:00.000', TIMESTAMP '2015-01-13 14:23:00.000', 'system', NULL, NULL)
+--,(1, 'emphasis', TIMESTAMP '2014-08-25 03:39:15.106', TIMESTAMP '2014-08-25 03:39:15.106', 'owner', NULL, NULL)
+--,(2, 'blockquotes', TIMESTAMP '2014-11-03 19:05:59.134', TIMESTAMP '2014-11-03 19:05:59.134', 'owner', NULL, NULL)
+--,(3, 'links', TIMESTAMP '2014-11-03 19:07:00.33', TIMESTAMP '2014-11-03 19:09:38.441', 'owner', 'owner', NULL)
+--,(4, 'images', TIMESTAMP '2014-11-03 19:08:24.767', TIMESTAMP '2014-11-03 19:08:24.767', 'owner', NULL, NULL)
+--,(5, 'headers', TIMESTAMP '2014-11-03 19:08:24.767', TIMESTAMP '2014-11-03 19:08:24.767', 'owner', NULL, NULL)
+--,(6, 'lists', TIMESTAMP '2014-11-03 19:08:24.767', TIMESTAMP '2014-11-03 19:08:24.767', 'owner', NULL, NULL)
+--,(7, 'code spans', TIMESTAMP '2014-11-03 19:08:24.767', TIMESTAMP '2014-11-03 19:08:24.767', 'owner', NULL, NULL)
+--,(8, 'empty', TIMESTAMP '2014-11-03 19:08:24.767', TIMESTAMP '2014-11-03 19:08:24.767', 'owner', NULL, NULL)
+--,(11, 'inline', TIMESTAMP '2014-11-03 19:08:24.767', TIMESTAMP '2014-11-03 19:08:24.767', 'owner', NULL, NULL)
+--,(12, 'reference', TIMESTAMP '2014-11-03 19:08:24.767', TIMESTAMP '2014-11-03 19:08:24.767', 'owner', NULL, NULL)
+--,(13, 'etc', TIMESTAMP '2014-11-03 19:08:24.767', TIMESTAMP '2014-11-03 19:08:24.767', 'owner', NULL, NULL)
+--,(100, 'markdown', TIMESTAMP '2014-11-03 19:08:24.767', TIMESTAMP '2014-11-03 19:08:24.767', 'owner', NULL, NULL)
+--;
 
 INSERT INTO TAG2TAG(TAG2TAG_ID, PARENT_ID, CHILD_ID) VALUES
 (1, 100, 1)
@@ -200,9 +365,14 @@ INSERT INTO TAG2FRAGMENT(TAG2FRAGMENT_ID, TAG_ID, FRAGMENT_ID) VALUES
 ,(21, 0, 16)
 ;
 
-INSERT INTO FRAGMENT2FRAGMENT(FRAGMENT2FRAGMENT_ID, FROM_ID, TO_ID, CREATION_DATETIME, UPDATE_DATETIME, CREATOR, UPDATER, PRIORITY) VALUES
-(1, 1, 2, TIMESTAMP '2014-11-03 19:07:21.641', TIMESTAMP '2014-11-03 19:07:21.641', 'owner', NULL, 0)
-,(2, 2, 1, TIMESTAMP '2014-11-03 19:07:21.644', TIMESTAMP '2014-11-03 19:07:21.644', 'owner', NULL, 0)
-,(3, 2, 4, TIMESTAMP '2014-11-03 19:08:51.216', TIMESTAMP '2014-11-03 19:08:51.216', 'owner', NULL, 0)
+INSERT INTO FRAGMENT2FRAGMENT(FRAGMENT2FRAGMENT_ID, FROM_ID, TO_ID) VALUES
+(1, 1, 2)
+,(2, 2, 1)
+,(3, 2, 4)
 ;
+--INSERT INTO FRAGMENT2FRAGMENT(FRAGMENT2FRAGMENT_ID, FROM_ID, TO_ID, CREATION_DATETIME, UPDATE_DATETIME, CREATOR, UPDATER, PRIORITY) VALUES
+--(1, 1, 2, TIMESTAMP '2014-11-03 19:07:21.641', TIMESTAMP '2014-11-03 19:07:21.641', 'owner', NULL, 0)
+--,(2, 2, 1, TIMESTAMP '2014-11-03 19:07:21.644', TIMESTAMP '2014-11-03 19:07:21.644', 'owner', NULL, 0)
+--,(3, 2, 4, TIMESTAMP '2014-11-03 19:08:51.216', TIMESTAMP '2014-11-03 19:08:51.216', 'owner', NULL, 0)
+--;
 
