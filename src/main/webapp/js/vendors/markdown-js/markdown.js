f25cfa15c7c0d2962769734599b377253be7e1dc
suewonjp <suewonjp@yahoo.co.jp>
2015-03-12 20:40:27 +0900

    [update][library] markdown.js; let the library NOT escape HTML tags;

--- src/main/webapp/js/vendors/markdown-js/markdown.js
+++ src/main/webapp/js/vendors/markdown-js/markdown.js
@@ -433,8 +433,8 @@
 
   function escapeHTML( text ) {
     return text.replace( /&/g, "&amp;" )
-               .replace( /</g, "&lt;" )
-               .replace( />/g, "&gt;" )
+//               .replace( /</g, "&lt;" )
+//               .replace( />/g, "&gt;" )
                .replace( /"/g, "&quot;" )
                .replace( /'/g, "&#39;" );
   }
