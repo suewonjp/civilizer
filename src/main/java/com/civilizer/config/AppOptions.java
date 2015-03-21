ead435466bf1eaea73e902cc6d74fdeab86c9be9
suewonjp <suewonjp@yahoo.co.jp>
2015-03-21 21:25:38 +0900

    [new][class] AppOptions; just a simple holder to keep application options strings

---
+++ src/main/java/com/civilizer/config/AppOptions.java
@@ -0,0 +1,9 @@
+package com.civilizer.config;
+
+public final class AppOptions {
+
+	public static final String PRIVATE_HOME_PATH        = "civilizer.private_home_path";
+	public static final String DB_FILE_PREFIX           = "civilizer.db_file_prefix";
+	public static final String UPLOADED_FILES_HOME      = "civilizer.uploaded_files_home";
+	
+}
