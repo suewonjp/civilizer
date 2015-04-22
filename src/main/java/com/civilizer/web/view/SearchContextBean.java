36cf1e8eb3f7bacc9890f7bf62f6a286e35ec0d1
suewonjp <suewonjp@gmail.com>
2015-04-22 16:23:05 +0900

    [update][experiment] searching fragments; base code for normal search functionality

--- src/main/java/com/civilizer/web/view/SearchContextBean.java
+++ src/main/java/com/civilizer/web/view/SearchContextBean.java
@@ -8,6 +8,13 @@ import com.civilizer.domain.SearchParams;
 public final class SearchContextBean implements Serializable {
 	
 	private String quickSearchText = "";
+	private String tagKeywords = "";
+	private String titleKeywords = "";
+	private String contentKeywords = "";
+	private String idKeywords = "";
+	private boolean anyTag;
+	private boolean anyTitle;
+	private boolean anyContent;
 	private int panelId = -1;
 	
 	public SearchContextBean() {
@@ -28,13 +35,69 @@ public final class SearchContextBean implements Serializable {
 	public void setQuickSearchText(String quickSearchText) {
 		this.quickSearchText = quickSearchText;
 	}
-	
-	public SearchParams buildSearchParams() {
-		if (! quickSearchText.isEmpty()) {
-			return new SearchParams(quickSearchText);
-		}
-		// [TODO] build search parameters from the various data collected with the view layer
-		return null;
-	}
+
+    public String getTagKeywords() {
+        return tagKeywords;
+    }
+
+    public void setTagKeywords(String tagKeywords) {
+        this.tagKeywords = tagKeywords;
+    }
+
+    public String getTitleKeywords() {
+        return titleKeywords;
+    }
+
+    public void setTitleKeywords(String titleKeywords) {
+        this.titleKeywords = titleKeywords;
+    }
+
+    public String getContentKeywords() {
+        return contentKeywords;
+    }
+
+    public void setContentKeywords(String contentKeywords) {
+        this.contentKeywords = contentKeywords;
+    }
+
+    public String getIdKeywords() {
+        return idKeywords;
+    }
+
+    public void setIdKeywords(String idKeywords) {
+        this.idKeywords = idKeywords;
+    }
+
+    public boolean isAnyTag() {
+        return anyTag;
+    }
+
+    public void setAnyTag(boolean anyTag) {
+        this.anyTag = anyTag;
+    }
+
+    public boolean isAnyTitle() {
+        return anyTitle;
+    }
+
+    public void setAnyTitle(boolean anyTitle) {
+        this.anyTitle = anyTitle;
+    }
+
+    public boolean isAnyContent() {
+        return anyContent;
+    }
+
+    public void setAnyContent(boolean anyContent) {
+        this.anyContent = anyContent;
+    }
+    
+    public SearchParams buildSearchParams() {
+        if (! quickSearchText.isEmpty()) {
+            return new SearchParams(quickSearchText);
+        }
+        // [TODO] build search parameters from the various data collected with the view layer
+        return null;
+    }
 
 }
