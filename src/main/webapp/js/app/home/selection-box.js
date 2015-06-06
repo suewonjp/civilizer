7e796f95927c90c99d374fbd1130cba133b8b547
suewonjp <suewonjp@yahoo.co.jp>
2015-06-06 09:40:43 +0900

    [update][refactoring][UI] context menus for selection box;

--- src/main/webapp/js/app/home/selection-box.js
+++ src/main/webapp/js/app/home/selection-box.js
@@ -8,19 +8,21 @@ function setContextMenuForSelections() {
     var menu = $("#selection-box-context-menu");
     
     $(".each-selected-frg, #selection-box-form\\:selection-box-panel").bind("contextmenu", function(event) {
-        showPopup(menu, event);
         var target = $(event.target).closest(".each-selected-frg");
         if (isNaN(parseInt(target.attr("_fid")))) {
             menu.find("#selection-box-form\\:bookmark").show();
             menu.find("#selection-box-form\\:relate").show();
             menu.find("#selection-box-form\\:trash").show();
             menu.find("#selection-box-form\\:select_unselect").hide();
+            if (listSelectedFragments())
+                showPopup(menu, event);
         }
         else {
             menu.find("#selection-box-form\\:bookmark").hide();
             menu.find("#selection-box-form\\:relate").hide();
             menu.find("#selection-box-form\\:trash").hide();
             menu.find("#selection-box-form\\:select_unselect").show();
+            showPopup(menu, event);
         }
             
         menu.data("target-fragment", target);
@@ -32,8 +34,15 @@ function setContextMenuForSelections() {
     });
 }
 
-function confirmBookmarkingSelectedFragments() {
-    
+function listSelectedFragments() {
+    var output = "";
+    $("#selection-box-form\\:selection-box-panel").find(".each-selected-frg").each(function() {
+        var $this = $(this);
+        if ($this.hasClass("middle-line"))
+            return;
+        output += "\n#" + $this.attr("_fid") + "  " + $this.attr("_ft") + "\n";
+    });
+    return output;
 }
 
 function confirmRelatingSelectedFragments() {
@@ -45,8 +54,9 @@ function confirmTrashingSelectedFragments() {
         document.forms["fragment-group-form"]["fragment-group-form:ok-trash-fragments"].click();
     });
     
-    var mainMsg = MSG.confirm_trashing; 
-    showConfirmDlg(mainMsg, null, "fa-trash", "orangered");
+    var mainMsg = MSG.confirm_trashing;
+    var subMsg = listSelectedFragments();
+    showConfirmDlg(mainMsg, subMsg, "fa-trash", "orangered");
 }
 
 function selectOrUnselectFragment() {
