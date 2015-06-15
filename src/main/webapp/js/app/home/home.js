ac83a96ff69a3913ce84835f19e3c739b1afe7f3
suewonjp <suewonjp@yahoo.co.jp>
2015-06-15 15:19:46 +0900

    [new][feature] now double clicking on each fragment header makes the fragment content toggle;

--- src/main/webapp/js/app/home/home.js
+++ src/main/webapp/js/app/home/home.js
@@ -206,12 +206,18 @@ function setupFragmentCheckboxes() {
 }
 
 function setupFragmentResolutionSliders() {
+    $("#fragment-group .fragment-header").dblclick(function() {
+        var tgt = $(this).closest(".each-fragment").find(".fragment-content");
+        showOrHide(tgt, !tgt.is(":visible"));
+    });
+    
     var settings = {
         max:10, min:0, value:10,
         change:function(event, ui) {
 //            console.log(ui.value);
         }
     };
+    
     $("#frg-reso-slider0, #frg-reso-slider1, #frg-reso-slider2").slider(settings);
 }
 
