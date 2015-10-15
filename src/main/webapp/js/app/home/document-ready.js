522d8b4563426779955c220525fbe113ef6c5030
suewonjp <suewonjp@gmail.com>
2015-10-15 15:15:48 +0900

    [update] shortcut keys for panel toggle; alt+1, alt+2, alt+3 will toggle panel 0, panel 1, panel 2 respectively; ctrl+space will show only one panel at a time;

--- src/main/webapp/js/app/home/document-ready.js
+++ src/main/webapp/js/app/home/document-ready.js
@@ -49,14 +49,44 @@ $(window).load(function() {
     setupFragmentCheckboxes();
 });
 
+var curPanel = 0;
+
 function onGlobalHotkeys(e) {
-    switch (e.keyCode) {
+    switch (parseInt(e.keyCode)) {
     case $.ui.keyCode.SPACE:
         if (e.ctrlKey && e.shiftKey) {
             if (!PF("searchDlg").isVisible()) {
                 showSearchDialog(1, null);
                 return false;
             }
+        }
+        else if (e.ctrlKey) {
+            if ($("#fragment-panel-0").is(":visible"))
+                $("#panel-toggler-0").click();
+            if ($("#fragment-panel-1").is(":visible"))
+                $("#panel-toggler-1").click();
+            if ($("#fragment-panel-2").is(":visible"))
+                $("#panel-toggler-2").click();
+            $("#panel-toggler-" + (++curPanel)%3).click();
+            return false;
+        }
+        break;
+    case 49:
+        if (e.altKey) {
+            $("#panel-toggler-0").click();
+            return false;
+        }
+        break;
+    case 50:
+        if (e.altKey) {
+            $("#panel-toggler-1").click();
+            return false;
+        }
+        break;
+    case 51:
+        if (e.altKey) {
+            $("#panel-toggler-2").click();
+            return false;
         }
         break;
     }
