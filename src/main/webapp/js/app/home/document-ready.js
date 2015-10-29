cbd2570fe191c32ac2f34aa200e1e26386b2fb36
suewonjp <suewonjp@gmail.com>
2015-10-29 16:51:47 +0900

    [update] changed shortcut keys for panel toggle; ctrl+space enabled one panel at a time previously; now alt+4 will does that;

--- src/main/webapp/js/app/home/document-ready.js
+++ src/main/webapp/js/app/home/document-ready.js
@@ -53,14 +53,34 @@ var curPanel = 0;
 
 function onGlobalHotkeys(e) {
     switch (parseInt(e.keyCode)) {
-    case $.ui.keyCode.SPACE:
+    case $.ui.keyCode.SPACE: // Ctrl + Shift + Space; show the search dialog
         if (e.ctrlKey && e.shiftKey) {
             if (!PF("searchDlg").isVisible()) {
                 showSearchDialog(1, null);
                 return false;
             }
         }
-        else if (e.ctrlKey) {
+        break;
+    case 49: // Alt + 1; toggle panel 0
+        if (e.altKey) {
+            $("#panel-toggler-0").click();
+            return false;
+        }
+        break;
+    case 50: // Alt + 2; toggle panel 1
+        if (e.altKey) {
+            $("#panel-toggler-1").click();
+            return false;
+        }
+        break;
+    case 51: // Alt + 3; toggle panel 2
+        if (e.altKey) {
+            $("#panel-toggler-2").click();
+            return false;
+        }
+        break;
+    case 52: // Alt + 4; ; enable a panel and disable all others in turn
+        if (e.altKey) {
             if ($("#fragment-panel-0").is(":visible"))
                 $("#panel-toggler-0").click();
             if ($("#fragment-panel-1").is(":visible"))
@@ -68,24 +88,6 @@ function onGlobalHotkeys(e) {
             if ($("#fragment-panel-2").is(":visible"))
                 $("#panel-toggler-2").click();
             $("#panel-toggler-" + (++curPanel)%3).click();
-            return false;
-        }
-        break;
-    case 49:
-        if (e.altKey) {
-            $("#panel-toggler-0").click();
-            return false;
-        }
-        break;
-    case 50:
-        if (e.altKey) {
-            $("#panel-toggler-1").click();
-            return false;
-        }
-        break;
-    case 51:
-        if (e.altKey) {
-            $("#panel-toggler-2").click();
             return false;
         }
         break;
