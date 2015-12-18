suewonjp/civilizer

commit f6a42f9f7406a3a7fcf119288e40ec9e9771686b

[update] now invoking fragment context menu has got a little bit easier;

src/main/webapp/js/app/home/contextmenu.js
@@ -2,13 +2,13 @@ function setContextMenuForFragments() {
     var menu = $("#frg-context-menu");
     
     $("#fragment-group, #fragment-overlay-content")
-    .off("contextmenu.cvz_frg").on("contextmenu.cvz_frg", ".fragment-header", function(e) {
+    .off("contextmenu.cvz_frg").on("contextmenu.cvz_frg", ".fragment-header, .fragment-header span", function(e) {
    
         var target = $(e.target);
-        if (target.hasClass("rclick-hint")) {
-            target = target.closest(".fragment-header");            
+        if (target.hasClass("rclick-hint") || target.is("span")) {
+            target = target.closest(".fragment-header");
         }
-        if (target.hasClass("fragment-header")) {
+        if (target && target.hasClass("fragment-header")) {
             showPopup(menu, e);
             menu.data("target-frg", target);
             if (target.attr("_deletable") === "true") {

commit 7d7c8ae74d67d51b149453edcf89c443e9f7eccf

[bug fix] invoking tag context menu on fragment headers had a glitch caused by the commit f6a42f9;

src/main/webapp/js/app/home/contextmenu.js
@@ -3,8 +3,9 @@ function setContextMenuForFragments() {
     
     $("#fragment-group, #fragment-overlay-content")
     .off("contextmenu.cvz_frg").on("contextmenu.cvz_frg", ".fragment-header, .fragment-header span", function(e) {
-   
         var target = $(e.target);
+        if (target.closest(".each-tag").length)
+            return true;
         if (target.hasClass("rclick-hint") || target.is("span")) {
             target = target.closest(".fragment-header");
         }