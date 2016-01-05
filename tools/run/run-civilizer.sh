506952a4ca98964a18e5684394fd382832af9f02
suewonjp <suewonjp@gmail.com>
2016-01-05 20:11:30 +0900

    [bug fix] running civilizer from *nix shell script didn't work; the cause was not clear; Bash's shell variable expansion with quotation marks didn't work as expected;

--- tools/run/run-civilizer.sh
+++ tools/run/run-civilizer.sh
@@ -27,11 +27,11 @@ done
 
 setupClasspath 
 
-homeOption=${home:+-Dcivilizer.private_home_path="$home"}
+homeOption="${home:+-Dcivilizer.private_home_path=$home}"
 
 cd "$extraPath/../"
 
 echo "[ $hostScript ] : Loading Civilizer..."
 java -Dorg.eclipse.jetty.util.log.class=org.eclipse.jetty.util.log.StdErrLog \
- -Dorg.eclipse.jetty.LEVEL=INFO \
- -cp "$classPath" "$homeOption" com.civilizer.extra.tools.Launcher --port $port
+ -Dorg.eclipse.jetty.LEVEL=INFO $homeOption \
+ -cp "$classPath" com.civilizer.extra.tools.Launcher --port $port
